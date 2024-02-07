2f10b4ddfe5143cb6366435bf68dd60694c0bfdb
Lars Walther <larswalther@Laptop-von-Lars.fritz.box>
2024-02-07 18:40:15 +0100

    Datenbankservice zur vorläufigen Registrierung eines Abonnenenten

--- services.py
+++ services.py
@@ -1,6 +1,8 @@
 import os
+import hashlib
+import random
 from models import Base, Abo, Subscriber, ResultModel
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, select, inspect
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import sessionmaker
 
@@ -12,10 +14,16 @@ Session = sessionmaker(bind=engine)
 if not database_exists:
     Base.metadata.create_all(engine)
 
-class DatabaseError(Exception):
-    """Exception wird bei Datenbankfehlern geworfen"""
-    pass
+def generate_retcode(email):
+    random_number = random.randint(10000, 99999)
+    combined_string = f"{email}{random_number}"
+    hash_object = hashlib.sha256(combined_string.encode())
+    retcode = hash_object.hexdigest()
+    return retcode
 
+def send_subscriber_email(abo_data):
+    print('in dieser Funktion wird die E-Mail gesendet')
+    #TODO Implementierung einer Funktion für das Senden einer E-Mail
 
 def insert_abo_data(abo_data):
 
@@ -42,14 +50,36 @@ def insert_abo_data(abo_data):
 
 def insert_subscriber_data(abo_data):
 
+    session = Session()
+
+    subscriber = session.query(Subscriber).filter(Subscriber.email == abo_data['email']).all()
+    if subscriber:
+        ret = ResultModel(httpstatus = 400,
+                          message = """Für die angegebene E-Mail-Adresse wurde bereits ein Abonnement vorgemerkt. 
+                                       Bitte prüfen Sie Ihr E-Mail-Postfach auf den Erhalt eines Bestätigungslinks""")
+        return ret
+    abonnent = session.query(Abo).filter(Abo.email == abo_data['email']).all()
+    if abonnent:
+        ret = ResultModel(httpstatus = 400,
+                          message = """Für die angegebene E-Mail-Adresse existiert bereits ein Abonnement. 
+                                       Für eventuelle Änderungen müssten wir Sie aus Sicherheitsgründen bitten, 
+                                       Ihr Abonnement zunächst zu beenden und dann neu einzutragen.""")
+        return ret
+    retcode = generate_retcode(abo_data['email'])
+    abo_data['retcode'] = retcode
+
     try:
-        session = Session()
         user = Subscriber(**abo_data)
         session.add(user)
         session.commit()
+        send_subscriber_email(abo_data)
         ret = ResultModel(httpstatus = 200,
-                          message = "Ihre Daten wurden erfolgreich in unserer Datenbank gespeichert.",
-                          retcode = abo_data.get('retcode'))
+                          message = """Vielen Dank für Ihr Interesse an unserem Medienangebot.
+                                       Wir haben Ihnen eine E-Mail mit einem Aktivierungslink
+                                       an die von Ihnen angegebene E-Mail-Adresse gesendet.
+                                       Bitte bestätigen Sie uns Ihr Abonnement durch Klick
+                                       auf den Aktivierungslink.""",
+                          retcode = retcode)
         return ret
 
     except SQLAlchemyError as e:
